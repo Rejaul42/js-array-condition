faa854c8c9fba21d1dae324c5cf5625db994c8a3
Rejaul42 <rejaulkarimraju91@gmail.com>
2023-07-29 22:57:40 -0600

    changes element value using index number

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var numbers = [76, 98, 65, 90, 23, 45, 80];
 
+// when you changes the specific element value
+
+numbers[4]=50;
+numbers[0]=40;
+console.log(numbers);
+
 // When you want to know the specific index of element
 var element = numbers[5];
 console.log(element);
