5a550c29a7201fea3f61173e9ebf427c42e35158
Rejaul42 <rejaulkarimraju91@gmail.com>
2023-07-29 22:53:36 -0600

    first commit

---
+++ array.js
@@ -0,0 +1,6 @@
+var friendsAge = [14, 21,20,18,26];
+
+/** when you want to know length of array*/
+
+var arrayLength = friendsAge.length;
+console.log(arrayLength);
---
+++ index.js
@@ -0,0 +1,10 @@
+var numbers = [76, 98, 65, 90, 23, 45, 80];
+
+// When you want to know the specific index of element
+var element = numbers[5];
+console.log(element);
+
+// When you want to know specific element of his index  
+
+var findIndex = numbers.indexOf(65);
+console.log(findIndex);
