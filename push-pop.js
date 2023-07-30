940aa55c0f2746d3488998d4755a739236975a8e
Rejaul42 <rejaulkarimraju91@gmail.com>
2023-07-29 23:26:07 -0600

    added pop, push, shift, unshift

---
+++ push-pop.js
@@ -0,0 +1,21 @@
+var numbers =[23, 57, 98, 65, 38, 98,];
+
+// push use for add an element last of an array.
+numbers.push(43);
+
+// unshift use for to add an element beginning of an array 
+numbers.unshift(10)
+console.log(numbers)
+
+//pop
+
+var names = ['abul', 'babul', 'kabul', 'chabul', 'dabul'];
+
+// pop use for remove last element of an array
+names.pop();
+console.log(names);
+
+// shift use for remove first element of an array
+names.shift()
+
+console.log(names);
